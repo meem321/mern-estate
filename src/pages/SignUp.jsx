02da1d9a36c4363665ef57854b0f2a143eb3ebbe
meem321 <hameemarrashid@gmail.com>
2023-10-19 09:20:59 +0600

    add redux toolkit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {Link,useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 export default function SignUp() {
@@ -16,8 +16,7 @@ export default function SignUp() {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      setLoading(true);
+    try {setLoading(true);
       const res = await fetch('api/auth/signup',
       {
         method: 'POST',
@@ -29,22 +28,23 @@ export default function SignUp() {
       const data = await res.json();
       console.log(data);
       if(data.success === false) {
-       setLoading(false);
-       setError(data.message);
-       return;
-      }     
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
       setLoading(false);
       setError(null);
       navigate('/sign-in');
     } catch (error) {
+
       setLoading(false);
       setError(error.message);
-    }
-  };
+    } 
+    };
   return (
     <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-    <form onSubmit={handleSubmit}className= 'flex flex-col gap-4'>
+    <form onSubmit={handleSubmit} className= 'flex flex-col gap-4'>
       <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'  onChange={handleChange}/>
       <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
       <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
